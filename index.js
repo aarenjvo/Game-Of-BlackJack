5fa50e62bd6c8dd90a664a06de4123152c0ddc27
Aaren Vo <aarenjvo@gmail.com>
2024-02-15 21:36:30 -0800

    Game is almost complete. Ace auto adjust still needs work.

--- index.js
+++ index.js
@@ -7,38 +7,58 @@ function showContent() {
 }
 
 function showGame() {
-    document.getElementById('dealerVsPlayer').style.display = 'block';
+    document.getElementById('gameStart').style.display = 'block';
     document.getElementById('hit').style.display = 'block';
     document.getElementById('stay').style.display = 'block';
 }
 
 function hideGame() {
-    document.getElementById('dealerVsPlayer').style.display = 'none';
+    document.getElementById('gameStart').style.display = 'none';
     // document.getElementById('hitStayWrap').style.display = 'none';
 }
 
-
 // BlackJack Game Logic
+
+// function resetGame() {
+//     document.getElementById('dealerHand').innerHTML = '';
+//     document.getElementById('playerHand').innerHTML = '';
+
+//     dealerSum = 0;
+//     playerSum = 0;
+
+//     dealerAceCount = 0;
+//     playerAceCount = 0;
+
+//     hiddenCard = null;
+//     deck = [];
+
+//     canHit = true;
+
+//     buildDeck();
+//     shuffleDeck();
+
+//     startGame();
+// }
 
 var dealerSum = 0;
 var playerSum = 0;
 
-var dealerAceValue = 0;
-var playerAceValue = 0;
+var dealerAceCount = 0;
+var playerAceCount = 0;
 
 var hiddenCard;
 var deck;
 
-var canHit = true; //will allow player to draw if playerSum is <= 21
+var canHit = true; //will allow player to draw if playerSum is < 21
 
 window.onload = function () {
     buildDeck();
     shuffleDeck();
     startGame();
-    // getValue();
-    // checkAce();
-}
+};
 
+
+// Create values and types related to image file names and run a for loop to create 52 card deck.
 function buildDeck() {
     let values = ['ACE', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
     let types = ['Clubs', 'Diamonds', 'Hearts', 'Spades'];
@@ -49,95 +69,105 @@ function buildDeck() {
             deck.push(values[j] + "_" + types[i]);
         }
     }
-}
+};
 
-//Function to have deck always randomized
+//Function to shuffle deck and always randomize.
+// Math.floor to round to lowest integer since math.random will generate a decimal number.
 function shuffleDeck() {
     for (let i = 0; i < deck.length; i++) {
         let j = Math.floor(Math.random() * deck.length);
-        let temp = deck[i];
+        let k = deck[i];
         deck[i] = deck[j];
-        deck[j] = temp;
+        deck[j] = k;
     }
     console.log(deck);
-}
+};
 
-//dealerSum adds the value of a card from the deck.
+//add value from whatever card is drawn from the deck to the dealerSum total.
 function startGame() {
     hiddenCard = deck.pop();
     dealerSum += getValue(hiddenCard);
+    // add value of the hidden card to dealer sum.
+    dealerAceCount += checkIfAce(hiddenCard);
+    let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "./assets/images/cards/" + card + ".png";
+        dealerSum += getValue(card);
+        dealerAceCount += checkIfAce(card);
+        document.getElementById("dealerHand").append(cardImg);
+        
 
-// deal cards to dealer hand
-    dealerAceValue += checkAce(hiddenCard);
-    console.log(hiddenCard);
-    console.log(dealerSum);
+    // deal cards to player hand
+    for (let i = 0; i < 2; i++) {
+        let cardImg = document.createElement("img");
+        let card = deck.pop();
+        cardImg.src = "./assets/images/cards/" + card + ".png";
+        playerSum += getValue(card);
+        playerAceCount += checkIfAce(card);
+        document.getElementById("playerHand").append(cardImg);
+    }
+    console.log("Your starting total: ", playerSum);
+    document.getElementById('hit').addEventListener("click", hit);
+    document.getElementById('stay').addEventListener("click", stay);
+};
+
+function hit() {
+    if (!canHit) {
+        return;
+    }
+    let cardImg = document.createElement("img");
+    let card = deck.pop();
+    cardImg.src = "./assets/images/cards/" + card + ".png";
+    playerSum += getValue(card);
+    playerAceCount += checkIfAce(card);
+    document.getElementById("playerHand").append(cardImg);
+
+    dealerSum = changeDealerAceValue(dealerSum, dealerAceCount);
+    playerSum = changePlayerAceValue(playerSum, playerAceCount);
+
+    if (changePlayerAceValue(playerSum, playerAceCount) > 21) {
+        canHit = false;
+        document.getElementById('hiddenCard').src = "./assets/images/cards/" + hiddenCard + ".png";
+
+        if (playerSum > 21) {
+            alert("You lose!");
+        }
+        else if (dealerSum > 21) {
+            alert("You win!");
+        }
+        else if (playerSum === dealerSum) {
+            alert("You and dealer push!");
+        }
+        else if (playerSum > dealerSum) {
+            alert("You win!");
+        }
+        else if (playerSum < dealerSum) {
+            alert("You lose!");
+        }
+
+        document.getElementById('dealer-Sum').innerText = dealerSum;
+        document.getElementById('player-Sum').innerText = playerSum;
+        console.log("Dealer's total: ", dealerSum);
+        console.log("Your total: ", playerSum);
+        resetGame();
+    }
+}
+
+function stay() {
+
     while (dealerSum < 17) {
         let cardImg = document.createElement("img");
         let card = deck.pop();
         cardImg.src = "./assets/images/cards/" + card + ".png";
         dealerSum += getValue(card);
-        dealerAceValue += checkAce(card);
+        dealerAceCount += checkIfAce(card);
         document.getElementById("dealerHand").append(cardImg);
     }
+    console.log(hiddenCard);
     console.log(dealerSum);
 
-    // deal cards to player hand
-    for(let i = 0; i < 2; i++) {
-        let cardImg = document.createElement("img");
-        let card = deck.pop();
-        cardImg.src = "./assets/images/cards/" + card + ".png";
-        playerSum += getValue(card);
-        playerAceValue += checkAce(card);
-        document.getElementById("playerHand").append(cardImg);
-    }
-    console.log(playerSum);
-    document.getElementById('hit').addEventListener("click", hit);
-    document.getElementById('stay').addEventListener("click", stay);
-}
-
-function hit() {
-    if(!canHit) {
-        return;
-    }
-    let cardImg = document.createElement("img");
-        let card = deck.pop();
-        cardImg.src = "./assets/images/cards/" + card + ".png";
-        playerSum += getValue(card);
-        playerAceValue += checkAce(card);
-        document.getElementById("playerHand").append(cardImg);
-
-        dealerSum = subtractAce(dealerSum, dealerAceValue);
-        playerSum = subtractAce(playerSum, playerAceValue);
-
-        if (subtractAce(playerSum, playerAceValue) > 21) {
-            canHit = false;
-            document.getElementById('hiddenCard').src = "./assets/images/cards/" + hiddenCard + ".png";
-
-            if (playerSum > 21) {
-                alert("You lose!");
-            }
-            else if (dealerSum > 21) {
-                alert("You win!");
-            }
-            else if (playerSum == dealerSum) {
-                alert("You and dealer push!");
-            }
-            else if (playerSum > dealerSum) {
-                alert("You win!");
-            }
-            else if (playerSum < dealerSum) {
-                alert("You lose!");
-            }
-        
-            document.getElementById('dealer-Sum').innerText = dealerSum;
-            document.getElementById('player-Sum').innerText = playerSum;
-        }
-}
-
-function stay() {
-    dealerSum = subtractAce(dealerSum, dealerAceValue);
-    playerSum = subtractAce(playerSum, playerAceValue);
-
+    dealerSum = changeDealerAceValue(dealerSum, dealerAceCount);
+    playerSum = changePlayerAceValue(playerSum, playerAceCount);
     canHit = false;
     document.getElementById('hiddenCard').src = "./assets/images/cards/" + hiddenCard + ".png";
 
@@ -147,7 +177,7 @@ function stay() {
     else if (dealerSum > 21) {
         alert("You win!");
     }
-    else if (playerSum == dealerSum) {
+    else if (playerSum === dealerSum) {
         alert("You and dealer push!");
     }
     else if (playerSum > dealerSum) {
@@ -159,12 +189,14 @@ function stay() {
 
     document.getElementById('dealer-Sum').innerText = dealerSum;
     document.getElementById('player-Sum').innerText = playerSum;
-    
+    console.log("Dealer's total: ", dealerSum);
+    console.log("Your total: ", playerSum);
+    resetGame();
+
 }
 
-//Pass in card parameter to getValue, split card into array and get numeric value of that first index.
+//Pass in a card parameter, split card into array and get the first index value of the split array.
 function getValue(card) {
-
     let data = card.split("_");
     let value = data[0];
     //Check if value is not a number, so that if not, 'ACE' can return 11 or 'J','Q','K' can return 10.
@@ -172,79 +204,132 @@ function getValue(card) {
         if (value == "ACE") {
             return 11;
         }
-        else return 10;
+        return 10;
     }
+    // numbers will be assumed as strings so parse it into an integer
     return parseInt(value);
 }
 
-function checkAce(card) {
+// Check if a card 
+function checkIfAce(card) {
     if (card[0] == "ACE") {
-        return 1;
-    }
-    return 0;
+        return 1
+    } return 0;
 }
 
 // allow ACE card to adjust to a value of 1
-function subtractAce(playerSum, playerAceValue) {
-    while (playerSum > 21 && playerAceValue > 0) {
+function changePlayerAceValue(playerSum, playerAceCount) {
+    while (playerSum > 21 && playerAceCount > 0) {
         playerSum -= 10;
-        playerAceValue -= 1;
+        playerAceCount -= 1;
     }
     return playerSum;
 }
 
+function changeDealerAceValue(dealerSum, dealerAceCount) {
+    while (dealerSum < 17 && dealerAceCount > 0) {
+        dealerSum -= 10;
+        dealerAceCount -= 1;
+    }
+    return dealerSum;
+}
 
-// Create Bet Counter
-var count = document.getElementById('betTotal');
+// Create bet counter
+
+// Add event listeners to the "Submit" button and the bet counter buttons in your JavaScript code.
+// Implement functions to handle increasing and decreasing bets when the respective buttons are clicked.
+// Implement logic in the "Submit" button's event listener to execute the game logic (deal cards, determine winner, update bank balance) when clicked.
+// Update the bank balance based on the game outcome (win or lose).
+
+function determineWinner(playerSum, dealerSum) {
+
+    if (playerSum > 21) {
+        return false;
+    }
+    else if (dealerSum > 21 || playerSum > dealerSum) {
+        return true;
+    }
+    else if (playerSum === dealerSum) {
+        return null;
+    }
+    else if (playerSum > dealerSum) {
+        return true;
+    }
+    else if (playerSum < dealerSum) {
+        return false;
+    }
+};
+
+function updateBankTotal(betOutcome) {
+    if (betOutcome === true) {
+        betTotal += wagerAmount;
+    } else if (betOutcome === false) {
+        betTotal -= wagerAmount;
+    }
+    document.getElementById('playerBank').textContent = betTotal;
+}
+
+document.getElementById('submitButton').addEventListener('click', function() {
+    if (!numberAppended) {
+    let wagerPlaced = document.getElementById('placedBetAmount');
+    let wagerAppended = document.createElement('p');
+        wagerAppended.textContent = wagerAmount;
+        wagerPlaced.appendChild(wagerAppended);
+
+        let winner = determineWinner(playerSum, dealerSum);
+        updateBankTotal(winner);
+
+        resetGame();
+
+        numberAppended = true;
+    }
+})
+
+var count = document.getElementById('playerBank');
 var p = document.createElement('p');
-p.textContent = 500;
-count.appendChild(p);
 
+let numberAppended = false;
 
 let betTotal = 500;
 
 let canBet = true;
 
-let wagerAmount = 100; // Initial bet amount
+let wagerAmount = 0; // Initial bet amount
 
-document.getElementById('subtractToBet').onclick = function decreaseBet() {
+document.getElementById('subtractBet').onclick = function decreaseBet() {
     if (wagerAmount > 0) {
-    wagerAmount -= 100;
-    document.getElementById('wagerAmount').innerHTML = wagerAmount;
+        wagerAmount -= 100;
+        document.getElementById('wagerAmount').innerHTML = wagerAmount;
     }
 }
 
-document.getElementById('addToBet').onclick = function increaseBet() {
-    if (canBet) { wagerAmount += 100;
+document.getElementById('addBet').onclick = function increaseBet() {
+    if (canBet) {
+        wagerAmount += 100;
         if (wagerAmount >= betTotal) {
             canBet = false;
         }
-    document.getElementById('wagerAmount').innerHTML = wagerAmount;
+        document.getElementById('wagerAmount').innerHTML = wagerAmount;
+    }
 }
-}
 
-// function resetGame() 
-//     dealerSum = 0;
-//     playerSum = 0;
-//     dealerAceValue = 0;
-//     playerAceValue = 0;
-//     canHit = true;
+// document.getElementById('submitButton').addEventListener('click', function() {
+    
 
-//     // Clear dealer and player hands
-//     document.getElementById("dealerHand").innerHTML = "";
-//     document.getElementById("playerHand").innerHTML = "";
+//     let winner = determineWinner(playerSum, dealerSum);
+//     if (winner === true) {
+//         betTotal += wagerAmount;
+//     } else if (winner === false) {
+//         betTotal -= wagerAmount;
+//     }
+//     document.getElementById('playerBank').textContent = betTotal;
+// });
 
-//     // Reset hidden card
-//     hiddenCard = deck.pop();
-//     dealerSum += getValue(hiddenCard);
-//     dealerAceValue += checkAce(hiddenCard);
+// var count = document.getElementById('playerBank');
+// var p = document.createElement('p');
+// p.textContent = 500;
+// count.appendChild(p);
 
-//     // Deal initial cards
-//     startGame();
 
-//     // Reset UI
-//     document.getElementById('hiddenCard').src = "";
-//     document.getElementById('dealer-Sum').innerText = "";
-//     document.getElementById('player-Sum').innerText = "";
-// }
-// document.getElementById('refresh').addEventListener("click", resetGame);
+
+
